9e0b239efe89970e1e0fbbcee6eae1562675af4d
heroncarli <eng.heroncarli@gmail.com>
2024-09-30 09:19:20 -0300

    alteracoes p deploy

--- frontend/cd-spa/vite.config.js
+++ frontend/cd-spa/vite.config.js
@@ -3,4 +3,9 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['react-icons/go', 'react-icons/bi', 'react-icons/ai']
+    }
+  }
 });
